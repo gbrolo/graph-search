1384998248b3da65cb57effc9f48a25ff5f87619
Gabriel Brolo Tobar <gabrolo13@gmail.com>
2019-02-05 09:55:12 -0600

    Sudoku board UI

--- app/components/PuzzleBoard/PuzzleBoard.js
+++ app/components/PuzzleBoard/PuzzleBoard.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { Button, Fade } from 'reactstrap';
+import { Button, Fade, Row, Col, Input } from 'reactstrap';
 
 import './puzzle_board.css';
+
+const colStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  alignContent: 'center'    
+}
 
 class SudokuBoard extends Component {
   constructor(props) {
@@ -13,9 +21,30 @@ class SudokuBoard extends Component {
 
   render() {
     return (
-      <Fade in={true} timeout={600}>
+      <Fade in={true} timeout={600} style={{ width: '100%', height: '50%' }}>
         <div className="puzzle-wrapper">
-          15-puzzle
+          <Row style={{
+              width: '50%',
+              marginTop: 20
+          }}> 
+              <Col xs={6} sm={6} md={6} lg={6} style={colStyle}>
+                  <Input className="abel-font text-uppercase" type="text" name="input" id="input" placeholder="Enter string to test" />
+              </Col>
+              <Col xs={6} sm={6} md={6} lg={6} style={colStyle}>
+                  <Button className="txt-uppercase abel-font" outline>Test string</Button>
+              </Col>
+          </Row>
+          <Row style={{
+              width: '100%',
+              height: '100%'
+          }}>
+              <Col xs={6} sm={6} md={6} lg={6} style={colStyle}>
+                  Board
+              </Col>
+              <Col xs={6} sm={6} md={6} lg={6} style={colStyle}>
+                  Output
+              </Col>
+          </Row>
         </div>
       </Fade>
     );
--- app/components/SudokuBoard/SudokuBoard.js
+++ app/components/SudokuBoard/SudokuBoard.js
@@ -1,21 +1,119 @@
 import React, { Component } from 'react';
-import { Button, Fade } from 'reactstrap';
+import { Button, Fade, Row, Col, Input } from 'reactstrap';
 
 import './sudoku.css';
+
+const colStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center'    
+}
+
+const boardCol = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center'        
+}
 
 class SudokuBoard extends Component {
   constructor(props) {
     super(props);    
 
     this.state = {
+        input: null,
+        cellValues: [
+            ['.', '4', '.', '1'],
+            ['3', '.', '4', '.'],
+            ['1', '.', '.', '4'],
+            ['.', '2', '1', '.']
+        ]
     }
   }
 
-  render() {
+  parseString() {
+      let array = this.state.input.split('');
+      let cellValues = [];
+
+      for (var i = 0; i < 4; i ++) {
+          let row = [];
+          for (var j = 0; j < 4; j++) {
+              row.push(array.shift());
+          }
+          cellValues.push(row);
+      }      
+
+      this.setState({ cellValues });
+  }
+
+  render() {      
     return (
-      <Fade in={true} timeout={600}>
+      <Fade in={true} timeout={600} style={{ width: '100%', height: '50%' }}>
         <div className="sudoku-wrapper">
-          Sudoku
+            <Row style={{
+                width: '50%',
+                marginTop: 20
+            }}> 
+                <Col xs={12} sm={12} md={6} lg={6} style={colStyle}>
+                    <Input 
+                        className="abel-font text-uppercase" 
+                        type="text" 
+                        name="input" 
+                        id="input" 
+                        placeholder="Enter string to test"
+                        onChange={(event) => this.setState({ input: event.target.value })} />
+                </Col>
+                <Col xs={12} sm={12} md={3} lg={3} style={colStyle}>
+                    <Button className="txt-uppercase abel-font" outline onClick={() => this.parseString()}>Apply string</Button>
+                </Col>
+                <Col xs={12} sm={12} md={3} lg={3} style={colStyle}>
+                    <Button className="txt-uppercase abel-font" outline>Test string</Button>
+                </Col>
+            </Row>
+            <Row style={{
+                width: '50%',
+                height: '100%',
+                marginTop: 20
+            }}>
+                <Col xs={12} sm={12} md={6} lg={6} style={boardCol}>
+                    <div className="board">
+                        {
+                            this.state.cellValues.map((row, index) => {                                
+                                return(
+                                    <Row key={index} style={{ width: '100%', height: 75 }}>
+                                    <Col xs={3} sm={3} md={3} lg={3} style={{ padding: 0, margin: 0, justifyContent: 'center', alignContent: 'center', alignItems: 'center', display: 'flex', width: '100%', height: '100%' }}>
+                                        <div className="sudoku-cell">
+                                            {row[0] === '.' ? '' : row[0]}
+                                        </div>
+                                    </Col>
+                                    <Col xs={3} sm={3} md={3} lg={3} style={{ padding: 0, margin: 0, justifyContent: 'center', alignContent: 'center', alignItems: 'center', display: 'flex', width: '100%', height: '100%' }}>
+                                        <div className="sudoku-cell">
+                                            {row[1] === '.' ? '' : row[1]}
+                                        </div>
+                                    </Col>
+                                    <Col xs={3} sm={3} md={3} lg={3} style={{ padding: 0, margin: 0, justifyContent: 'center', alignContent: 'center', alignItems: 'center', display: 'flex', width: '100%', height: '100%' }}>
+                                        <div className="sudoku-cell">
+                                            {row[2] === '.' ? '' : row[2]}
+                                        </div>
+                                    </Col>
+                                    <Col xs={3} sm={3} md={3} lg={3} style={{ padding: 0, margin: 0, justifyContent: 'center', alignContent: 'center', alignItems: 'center', display: 'flex', width: '100%', height: '100%' }}>
+                                        <div className="sudoku-cell">
+                                            {row[3] === '.' ? '' : row[3]}
+                                        </div>
+                                    </Col>
+                                </Row>
+                                )
+                            })
+                        }                        
+                    </div>
+                </Col>
+                <Col xs={12} sm={12} md={6} lg={6} style={colStyle}>
+                    Output
+                </Col>
+            </Row>
         </div>
       </Fade>
     );
